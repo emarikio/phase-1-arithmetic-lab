5312c769270508a8c0e38298178a91d8a122eda2
Emarilyne Wakio <emarilynew@gmail.com>
2024-04-14 14:00:29 +0300

    arithmetics

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 16;
+let num4 = 4;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(num1, num2, num3, num4, 20);
+
